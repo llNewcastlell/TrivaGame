a5c6ad34617e25d5c1e98a30d4c4d0e9234924a7
Newcastle <vaughnmarques@gmail.com>
2018-06-24 12:46:58 -0600

    first sunday push

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -47,27 +47,75 @@ $(document).ready(function () {
 
 
     ]
-    var intervalId;
+    
     var time = 60;
+    var correctAnswer;
+    var wrongAnswer;
+    var userSelected;
   
+
+    // for (i =0; i , questions.length; i++ ){
+
+    //         if(questions[i].question < 5);
+    //         $("#q1").append(questions[0].question + "<q2>");
+    //         $("#q2").append(questions[1].question);
+    //         $("#q3").append(questions[2].question);
+    //         $("#q4").append(questions[3].question);
+    //         $("#q5").append(questions[4].question);
+    //         $("#a1").append("<input type=radio name = radio1 checkstate = true> " + questions[0].choiceOne,  ": " +" <input type=radio name = radio1 checkstate = true> " + questions[0].choiceTwo + "<input type=radio name = radio1 checkstate = true> " + questions[0].choiceThree +  "<input type=radio name = radio1 checkstate = true> " + questions[0].choiceFour);
+            
+    //         $("#a2").append("<input type=radio name = radio1 checkstate = true> " + questions[1].choiceOne,  ": " +" <input type=radio name = radio1 checkstate = true> " + questions[1].choiceTwo + "<input type=radio name = radio1 checkstate = true> " + questions[1].choiceThree +  "<input type=radio name = radio1 checkstate = true> " + questions[1].choiceFour);
+
+    //         $("#a3").append("<input type=radio name = radio1 checkstate = true> " + questions[2].choiceOne,  ": " +" <input type=radio name = radio1 checkstate = true> " + questions[2].choiceTwo + "<input type=radio name = radio1 checkstate = true> " + questions[2].choiceThree +  "<input type=radio name = radio1 checkstate = true> " + questions[2].choiceFour);
+
+    //         $("#a4").append("<input type=radio name = radio1 checkstate = true> " + questions[3].choiceOne,  ": " +" <input type=radio name = radio1 checkstate = true> " + questions[3].choiceTwo + "<input type=radio name = radio1 checkstate = true> " + questions[3].choiceThree +  "<input type=radio name = radio1 checkstate = true> " + questions[3].choiceFour);
+
+    //         $("#a5").append("<input type=radio name = radio1 checkstate = true> " + questions[4].choiceOne,  ": " +" <input type=radio name = radio1 checkstate = true> " + questions[4].choiceTwo + "<input type=radio name = radio1 checkstate = true> " + questions[4].choiceThree +  "<input type=radio name = radio1 checkstate = true> " + questions[4].choiceFour);
+
+
+
+            
+
+            // console.log(question);
+
+            
+            
+            // document.write(questions[i].question);
+    // }
 // Diplay questions to the page
 
     function gamePlay() {
         $("#start").on('click', function() {
             
-            $("button").remove("#start");
+            $("#start").hide("#start");
            
 
-            $("#questions").append(questions[0].question + ": " +"<ul> <input type=radio name = radio1 checkstate = true> " + questions[0].choiceOne +","+"<input type=radio name = radio1 checkstate = false>" + questions[0].choiceTwo +"," +"<input type=radio name = radio1 checkstate = false>" + questions[0].choiceThree +"," + "<input type=radio name = radio1 checkstate = false>" + questions[0].choiceFour +"</ul>"+"<br>");
-            $("#questions").append(questions[1].question + ": " + "<ul> <input type=radio name = radio2 checkstate = false>" + questions[1].choiceOne +"," + "<input type=radio name = radio2 checkstate = false>" +questions[1].choiceTwo +"," + "<input type=radio name = radio2 checkstate = false>" +  questions[0].choiceThree +"," + "<input type=radio name = radio2 checkstate = false>" + questions[1].choiceFour +"</ul>"+"<br>");
-            $("#questions").append(questions[2].question + ": " + " <ul> <input type=radio name = radio3 checkstate = false>" + questions[2].choiceOne +","+ "<input type=radio name = radio3 checkstate = false>" + questions[2].choiceTwo +"," + "<input type=radio name = radio3 checkstate = false>" + questions[2].choiceThree +","+ "<input type=radio name = radio3 checkstate = false>" + questions[2].choiceFour + "</ul>"+ "<br>");
-            $("#questions").append(questions[3].question + ": " + " <ul><input type=radio name = radio4 checkstate = false>" + questions[3].choiceOne +","+ "<input type=radio name = radio4 checkstate = false>" + questions[3].choiceTwo +","+ "<input type=radio name = radio4 checkstate = false>" + questions[3].choiceThree +","+ "<input type=radio name = radio4 checkstate = false>" + questions[3].choiceFour +"</ul>"+ "<br>");
-            $("#questions").append(questions[4].question + ": " + " <ul> <input type=radio name = radio5 checkstate = false>" + questions[4].choiceOne +","+ "<input type=radio name = radio5 checkstate = false>" + questions[4].choiceTwo +","+ "<input type=radio name = radio5 checkstate = false>" + questions[4].choiceThree +","+ "<input type=radio name = radio5 checkstate = false>" + questions[4].choiceFour + "</ul>"+ "<br>");
+            $("#q1").append(questions[0].question + ": " +"<ul> <input type=radio name = radio1 checkstate = true> " + questions[0].choiceOne +","+"<input type=radio name = radio1 checkstate = false>" + questions[0].choiceTwo +"," +"<input type=radio name = radio1 checkstate = false>" + questions[0].choiceThree +"," + "<input type=radio name = radio1 checkstate = false>" + questions[0].choiceFour +"</ul>"+"<br>");
+            $("#q2").append(questions[1].question + ": " + "<ul> <input type=radio name = radio2 checkstate = false>" + questions[1].choiceOne +"," + "<input type=radio name = radio2 checkstate = false>" +questions[1].choiceTwo +"," + "<input type=radio name = radio2 checkstate = false>" +  questions[0].choiceThree +"," + "<input type=radio name = radio2 checkstate = false>" + questions[1].choiceFour +"</ul>"+"<br>");
+            $("#q3").append(questions[2].question + ": " + " <ul> <input type=radio name = radio3 checkstate = false>" + questions[2].choiceOne +","+ "<input type=radio name = radio3 checkstate = false>" + questions[2].choiceTwo +"," + "<input type=radio name = radio3 checkstate = false>" + questions[2].choiceThree +","+ "<input type=radio name = radio3 checkstate = false>" + questions[2].choiceFour + "</ul>"+ "<br>");
+            $("#q4").append(questions[3].question + ": " + " <ul><input type=radio name = radio4 checkstate = false>" + questions[3].choiceOne +","+ "<input type=radio name = radio4 checkstate = false>" + questions[3].choiceTwo +","+ "<input type=radio name = radio4 checkstate = false>" + questions[3].choiceThree +","+ "<input type=radio name = radio4 checkstate = false>" + questions[3].choiceFour +"</ul>"+ "<br>");
+            $("#q5").append(questions[4].question + ": " + " <ul> <input type=radio name = radio5 checkstate = false>" + questions[4].choiceOne +","+ "<input type=radio name = radio5 checkstate = false>" + questions[4].choiceTwo +","+ "<input type=radio name = radio5 checkstate = false>" + questions[4].choiceThree +","+ "<input type=radio name = radio5 checkstate = false>" + questions[4].choiceFour + "</ul>"+ "<br>");
 
+
+            startTimer();
         // startClock();
           
         });
     }
+
+//          Check answer
+    // ***********************************************
+    
+
+
+
+
+
+
+
+
+
+    // ***************************************************
 // Display Score
     function getResults(){
 
@@ -81,56 +129,61 @@ $(document).ready(function () {
 
     }
 
+    function startTimer(){
+        time = setInterval( 1000)
+        console.log(startTimer);
+    }
+
     // counter
 
-    var counter = {
+    // var counter = {
 
-        time: 60,
+    //     time: 60,
 
 
-        count: function(){
-            counter.time--;
-            console.log(time);
-            $("#display").text(time);
-        }
-        
-    }
-        setInterval(function(){
-            // console.log("=======" + time);
-            time --;
-        });
-    // function startClock(){
-    //   $("#display").append(time)
-    //     time--;
+    //     count: function(){
+    //         counter.time--;
+    //         console.log(time);
+    //         $("#display").text(time);
+    //     }
         
     // }
+    //     setInterval(function(){
+    //         // console.log("=======" + time);
+    //         time --;
+    //     });
+    // // function startClock(){
+    // //   $("#display").append(time)
+    // //     time--;
+        
+    // // }
     
    
-    function timeConverter(t) {
+    // function timeConverter(t) {
 
-        var minutes = Math.floor(t / 60);
-        var seconds = t - (minutes * 60);
+    //     var minutes = Math.floor(t / 60);
+    //     var seconds = t - (minutes * 60);
       
-        if (seconds < 10) {
-          seconds = "0" + seconds;
-        }
+    //     if (seconds < 10) {
+    //       seconds = "0" + seconds;
+    //     }
       
-        if (minutes === 0) {
-          minutes = "00";
-        }
-        else if (minutes < 10) {
-          minutes = "0" + minutes;
-        }
+    //     if (minutes === 0) {
+    //       minutes = "00";
+    //     }
+    //     else if (minutes < 10) {
+    //       minutes = "0" + minutes;
+    //     }
       
-        return minutes + ":" + seconds;
-      }
-      function count() {
+    //     return minutes + ":" + seconds;
+    //   }
+    //   function count() {
 
-        time++;
-        var converted = timeConverter(time);
-        $("#display").text(converted);
+    //     time++;
+    //     var converted = timeConverter(time);
+    //     $("#display").text(converted);
       
-      }
+    //   }
    
    
 
